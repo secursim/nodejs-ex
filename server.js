709986ba6eb92f8971e3d93d48c61ada9a306107
secursim <62434509+secursim@users.noreply.github.com>
2020-05-25 16:16:28 +0200

    aggiunti require

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var express = require('express'),
     app     = express(),
     morgan  = require('morgan');
     
-Object.assign=require('object-assign')
+Object.assign=require('object-assign');
 
 app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'))
+app.use(morgan('combined'));
+require('/views/Component.js');
+require('/views/manifest.json');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
@@ -79,19 +81,8 @@ app.get('/', function (req, res) {
   if (!db) {
     initDb(function(err){});
   }
-  if (db) {
-    var col = db.collection('counts');
-    // Create a document with request IP and current time of request
-    col.insert({ip: req.ip, date: Date.now()});
-    col.count(function(err, count){
-      if (err) {
-        console.log('Error running count. Message:\n'+err);
-      }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
-    });
-  } else {
-    res.render('index.html', { pageCountMessage : null});
-  }
+   res.render('index.html', { pageCountMessage : null});
+    
 });
 
 app.get('/pagecount', function (req, res) {
