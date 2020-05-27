9b334ee0323a891852ef7e193c8c9b9963c51747
secursim <62434509+secursim@users.noreply.github.com>
2020-05-27 14:34:49 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ Object.assign=require('object-assign')
 
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
-app.require('Component.js');
-app.require('manifest.json');
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
